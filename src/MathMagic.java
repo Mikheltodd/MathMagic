32d33b35a0e578d2f00a8e969ccdf323451b9f91
Mikheltodd <68166999+Mikheltodd@users.noreply.github.com>
2025-02-28 18:31:57 -0500

    Initial commit

---
+++ src/MathMagic.java
@@ -0,0 +1,14 @@
+public class MathMagic {
+    public static void main(String[] args) {
+        // Programa de número mágico
+        int myNumber = 23;
+        int stepOne = myNumber * myNumber;
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo / myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive / 6;
+        System.out.println(stepSix);
+
+    }
+}
